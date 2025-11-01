86e0603c8bfebbc0dd9f48221a37138b141f6c58
Donny <tanadornrach@gmail.com>
2025-11-02 01:28:00 +0700

    placeholder datasets and labels in ChartData objects

--- src/app/data/line_chart.ts
+++ src/app/data/line_chart.ts
@@ -1,4 +1,5 @@
 import { ChartData, ChartOptions } from "chart.js";
+import { rgba } from "polished";
 
 export const lineChartOptions: ChartOptions<"line"> = {
   responsive: true,
@@ -12,16 +13,18 @@ export const lineChartOptions: ChartOptions<"line"> = {
       color: "black",
     },
   },
-}
+};
 
 export const lineChartData: ChartData<"line"> = {
-  labels: [],
+  labels: ["26/10", "27/10", "28/10", "29/10", "30/10", "31/10", "1/11"],
   datasets: [
     {
-      data: [],
-      fill: false,
+      data: [7, 4, 4, 7, 8, 3, 2],
+      fill: true,
       borderColor: "blue",
-      tension: 0.1,
+      backgroundColor: rgba("blue", 0.5),
+      tension: 0.4,
+      label: "Number of Sales",
     },
   ],
 };

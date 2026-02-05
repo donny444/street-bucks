501d6406dd823901f1c9c0e825b43ebc93e41486
Donny <tanadornrach@gmail.com>
2026-02-05 18:51:11 +0700

    delete slop

--- src/components/forms/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Field: React.FC<{ label: string; children: React.ReactNode }> = ({ label, children }) => (
-  <label style={{ display: "block", marginBottom: 8 }}>
-    <div style={{ fontSize: 13, color: "#374151", marginBottom: 6 }}>{label}</div>
-    {children}
-  </label>
-);

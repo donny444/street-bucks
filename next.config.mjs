1c3dc33d7c3311283e6b65ad6ea0c293854bb606
Donny <tanadornrach@gmail.com>
2026-03-03 20:41:29 +0700

    include env and image sources to `nextConfig`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    NEXT_PUBLIC_DOCKER_SERVER_URL: process.env.NEXT_PUBLIC_DOCKER_SERVER_URL,
+  },
+  images: {
+    domains: [
+      process.env.NEXT_PUBLIC_SERVER_URL.replace(/^https?:\/\//, ""),
+      process.env.NEXT_PUBLIC_DOCKER_SERVER_URL.replace(/^https?:\/\//, ""),
+      "localhost",
+    ],
+  },
+};
 
 export default nextConfig;
